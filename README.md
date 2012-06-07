c01261d7431f1fef46ef7e4be767d312801c0168
Jakob Borg <jakob@nym.se>
2012-06-07 15:28:16 +0200

    DRYer names and cleanup.

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ iso8601
 [![build status](https://secure.travis-ci.org/calmh/node-iso8601.png)](http://travis-ci.org/calmh/node-iso8601)
 
     var iso8601 = require('iso8601');
-    var fromIso8601 = iso8601.fromIso8601;
-    var toIso8601 = iso8601.toIso8601;
     
     var d = new Date(1325471624000);
-    toIso8601(d); // => 2012-01-02T02:33:44Z
+    iso8601.fromDate(d); // => 2012-01-02T02:33:44Z
     
-    fromIso8601('2012-01-02T02:33:44Z'); // => new Date(1325471624000)
+    iso8601.toDate('2012-01-02T02:33:44Z'); // => new Date(1325471624000)
 
--- test/iso8601.js
+++ test/iso8601.js
@@ -1,8 +1,6 @@
 var should = require('should');
 
 var iso8601 = require('../lib/iso8601');
-var fromIso8601 = iso8601.fromIso8601;
-var toIso8601 = iso8601.toIso8601;
 
 describe('The ISO8601 library', function () {
     // jb@zdev:~ % date +%s -d "2012-01-02 02:33:44 UTC"
@@ -10,12 +8,12 @@ describe('The ISO8601 library', function () {
  
     it('produces a correct ISO8601 string for a given Date', function () {
         var d = new Date(1325471624000);
-        toIso8601(d).should.equal('2012-01-02T02:33:44Z');
+        iso8601.fromDate(d).should.equal('2012-01-02T02:33:44Z');
     });
 
     it('parses an ISO8601 string and returns the correct Date', function () {
         var d = new Date(1325471624000);
-        fromIso8601('2012-01-02T02:33:44Z').should.equal(d);
+        iso8601.toDate('2012-01-02T02:33:44Z').should.equal(d);
     });
 });
 
