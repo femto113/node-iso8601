9e035db49cf0cf3fbed5b002423b58468f12793e
Jakob Borg <jakob@nym.se>
2012-06-07 13:49:23 +0200

    Initial commit.

---
+++ test/iso8601.js
@@ -0,0 +1,21 @@
+var should = require('should');
+
+var iso8601 = require('../lib/iso8601');
+var fromIso8601 = iso8601.fromIso8601;
+var toIso8601 = iso8601.toIso8601;
+
+describe('The ISO8601 library', function () {
+    // jb@zdev:~ % date +%s -d "2012-01-02 02:33:44 UTC"
+    // 1325471624
+ 
+    it('produces a correct ISO8601 string for a given Date', function () {
+        var d = new Date(1325471624000);
+        toIso8601(d).should.equal('2012-01-02T02:33:44Z');
+    });
+
+    it('parses an ISO8601 string and returns the correct Date', function () {
+        var d = new Date(1325471624000);
+        fromIso8601('2012-01-02T02:33:44Z').should.equal(d);
+    });
+});
+
